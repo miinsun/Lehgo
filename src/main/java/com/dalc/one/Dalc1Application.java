1089ffc458f66d150438dc60a5faaa2038656b27
JisuKong <jisukong52@gmail.com>
2021-12-28 18:22:23 +0900

    init

---
+++ src/main/java/com/dalc/one/Dalc1Application.java
@@ -0,0 +1,13 @@
+package com.dalc.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dalc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dalc1Application.class, args);
+	}
+
+}
