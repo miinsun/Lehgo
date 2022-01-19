daf2bbf499e48c7858bf37737b0a9f617c210250
JisuKong <kongjisu52@gmail.com>
2022-01-20 08:53:58 +0900

    TEST: front 구현을 위한 임시 CORS 설정

--- src/main/java/com/dalc/one/WebMvcConfig.java
+++ src/main/java/com/dalc/one/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.dalc.one;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +10,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/static/", "classpath:/public/",
 			"classpath:/", "classpath:/resources/", "classpath:/META-INF/resources/",
 			"classpath:/META-INF/resources/webjars/", "classpath:/templates/", "classpath:/static/vue/"};
-
+	
+	//front 구현을 위한 임시 설정
+	@Override
+	  public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/**")
+	        .allowedOrigins("http://localhost:8081");
+	}
+	
 	@Override
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
