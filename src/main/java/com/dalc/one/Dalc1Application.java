94a928445ef21f5e8d6454bd5cadd82739423956
민선 <61786235+miinsun@users.noreply.github.com>
2022-03-17 18:45:17 +0900

    Update Dalc1Application.java

--- src/main/java/com/dalc/one/Dalc1Application.java
+++ src/main/java/com/dalc/one/Dalc1Application.java
@@ -5,28 +5,40 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
 
 @SpringBootApplication
 public class Dalc1Application {
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/static/", "classpath:/public/",
+			"classpath:/", "classpath:/resources/", "classpath:/META-INF/resources/",
+			"classpath:/META-INF/resources/webjars/", "classpath:/templates/", "classpath:/static/vue/"};
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Dalc1Application.class, args);
 	}
 	
 	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-    		.allowedOrigins("http://3.37.127.183:8081")
-	        .exposedHeaders("authorization")	//make client read header("jwt-token")
-	        .allowedMethods(
-	            	HttpMethod.GET.name(),
-	            	HttpMethod.HEAD.name(),
-	            	HttpMethod.POST.name(),
-	            	HttpMethod.PUT.name(),
-	            	HttpMethod.DELETE.name());
-            }
-        };
-    }
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+				.allowedOrigins("http://3.37.127.183:8081")
+				.exposedHeaders("authorization")	//make client read header("jwt-token")
+				.allowedMethods(
+				HttpMethod.GET.name(),
+				HttpMethod.HEAD.name(),
+				HttpMethod.POST.name(),
+				HttpMethod.PUT.name(),
+				HttpMethod.DELETE.name()
+			);
+		}
+	}
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+	}
 }
