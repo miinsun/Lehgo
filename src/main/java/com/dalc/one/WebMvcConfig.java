db9ae1c6d43764e94dac037031a37b4e3ececff2
민선 <61786235+miinsun@users.noreply.github.com>
2022-03-17 21:36:20 +0900

    Update WebMvcConfig.java

--- src/main/java/com/dalc/one/WebMvcConfig.java
+++ src/main/java/com/dalc/one/WebMvcConfig.java
@@ -1,44 +1,44 @@
-package com.dalc.one;
+// package com.dalc.one;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.http.HttpMethod;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/static/", "classpath:/public/",
-			"classpath:/", "classpath:/resources/", "classpath:/META-INF/resources/",
-			"classpath:/META-INF/resources/webjars/", "classpath:/templates/", "classpath:/static/vue/"};
+// @Configuration
+// public class WebMvcConfig implements WebMvcConfigurer {
+// 	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/static/", "classpath:/public/",
+// 			"classpath:/", "classpath:/resources/", "classpath:/META-INF/resources/",
+// 			"classpath:/META-INF/resources/webjars/", "classpath:/templates/", "classpath:/static/vue/"};
 	
-	//front 구현을 위한 임시 설정
-	@Override
-	  public void addCorsMappings(CorsRegistry registry) {
-// 	    registry.addMapping("/**")
-// 	     	.allowedHeaders("*")
-// 		.allowedOrigins("http://3.37.127.183:8080")
-// 		.allowedOrigins("http://3.37.127.183:8081")
-// 	        // .exposedHeaders("authorization")	//make client read header("jwt-token")
-// 	        .allowedMethods(
-// 	            	HttpMethod.GET.name(),
-// 	            	HttpMethod.HEAD.name(),
-// 	            	HttpMethod.POST.name(),
-// 	            	HttpMethod.PUT.name(),
-// 	            	HttpMethod.DELETE.name());
-//            ;
+// 	//front 구현을 위한 임시 설정
+// 	@Override
+// 	  public void addCorsMappings(CorsRegistry registry) {
+// // 	    registry.addMapping("/**")
+// // 	     	.allowedHeaders("*")
+// // 		.allowedOrigins("http://3.37.127.183:8080")
+// // 		.allowedOrigins("http://3.37.127.183:8081")
+// // 	        // .exposedHeaders("authorization")	//make client read header("jwt-token")
+// // 	        .allowedMethods(
+// // 	            	HttpMethod.GET.name(),
+// // 	            	HttpMethod.HEAD.name(),
+// // 	            	HttpMethod.POST.name(),
+// // 	            	HttpMethod.PUT.name(),
+// // 	            	HttpMethod.DELETE.name());
+// //            ;
 		  
-	  	 registry.addMapping("/**")
-			.allowedHeaders("*")
-			.allowedOrigins("http://3.37.127.183:8081")
-			.allowedMethods("*")
-			.allowCredentials(false);
-	}
+// 	  	 registry.addMapping("/**")
+// 			.allowedHeaders("*")
+// 			.allowedOrigins("http://3.37.127.183:8081")
+// 			.allowedMethods("*")
+// 			.allowCredentials(false);
+// 	}
 	
 	
 	
-	@Override
-	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-	}
-}
+// 	@Override
+// 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+// 		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+// 	}
+// }
