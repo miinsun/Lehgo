71baaeb8f17da1eaffbc997c4b47c2230667dde9
JisuKong <kongjisu52@gmail.com>
2022-03-17 17:53:12 +0900

    CORS IP 변경

--- src/main/java/com/dalc/one/WebMvcConfig.java
+++ src/main/java/com/dalc/one/WebMvcConfig.java
@@ -16,8 +16,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	  public void addCorsMappings(CorsRegistry registry) {
 	    registry.addMapping("/**")
-        	.allowedOrigins("http://3.37.127.183:8080/")
-        	.allowedOrigins("http://3.37.127.183:8081/")
+    	.allowedOrigins("http://3.37.127.183:8080")
+    	.allowedOrigins("http://3.37.127.183:8081")
 	        .exposedHeaders("authorization")	//make client read header("jwt-token")
 	        .allowedMethods(
 	            	HttpMethod.GET.name(),
