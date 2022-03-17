f50f88f3253d80b3fc1496a568f976e0184305a5
민선 <61786235+miinsun@users.noreply.github.com>
2022-03-17 20:31:37 +0900

    Update UserDTO.java

--- src/main/java/com/dalc/one/user/UserDTO.java
+++ src/main/java/com/dalc/one/user/UserDTO.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonAutoDetect
 public class UserDTO{
 	private String id;
 	private String password;
