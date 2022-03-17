4202a4d9093af2b1eafe86fb742a7c3c9ee14c4f
민선 <61786235+miinsun@users.noreply.github.com>
2022-03-17 19:49:06 +0900

    Update Dalc1Application.java

--- src/main/java/com/dalc/one/Dalc1Application.java
+++ src/main/java/com/dalc/one/Dalc1Application.java
@@ -15,34 +15,34 @@ public class Dalc1Application {
 		SpringApplication.run(Dalc1Application.class, args);
 	}
 	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/static/", "classpath:/public/",
-				"classpath:/", "classpath:/resources/", "classpath:/META-INF/resources/",
-				"classpath:/META-INF/resources/webjars/", "classpath:/templates/", "classpath:/static/vue/"};
+// 	@Bean
+// 	public WebMvcConfigurer corsConfigurer() {
+// 		final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/static/", "classpath:/public/",
+// 				"classpath:/", "classpath:/resources/", "classpath:/META-INF/resources/",
+// 				"classpath:/META-INF/resources/webjars/", "classpath:/templates/", "classpath:/static/vue/"};
 		
-		return new WebMvcConfigurer() {
+// 		return new WebMvcConfigurer() {
 			
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**")
-				.allowedOrigins("http://3.37.127.183:8081")
-				.exposedHeaders("authorization")	//make client read header("jwt-token")
-				.allowedMethods(
-				HttpMethod.GET.name(),
-				HttpMethod.HEAD.name(),
-				HttpMethod.POST.name(),
-				HttpMethod.PUT.name(),
-				HttpMethod.DELETE.name()
-			);
-			}
+// 			@Override
+// 			public void addCorsMappings(CorsRegistry registry) {
+// 			registry.addMapping("/**")
+// 				.allowedOrigins("http://3.37.127.183:8081")
+// 				.exposedHeaders("authorization")	//make client read header("jwt-token")
+// 				.allowedMethods(
+// 				HttpMethod.GET.name(),
+// 				HttpMethod.HEAD.name(),
+// 				HttpMethod.POST.name(),
+// 				HttpMethod.PUT.name(),
+// 				HttpMethod.DELETE.name()
+// 			);
+// 			}
 			
-			@Override
-			public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-				registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-			}
-		};
-	}
+// 			@Override
+// 			public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+// 				registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+// 			}
+// 		};
+// 	}
 	
 	
 }
